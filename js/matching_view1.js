2f2f2d32e001b5269f177fb644a08810e0fa7046
skmzk418 <skmzk418.scammyman@gmail.com>
2019-03-07 05:13:17 +0900

    共通パーツの構築

---
+++ js/matching_view1.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    flow:[
+      {state: true, title: 'ユーザーを選ぶ'},
+      {state: false, title: 'マッチング方法<br>を選ぶ'},
+      {state: false, title: '確認する'},
+      {state: false, title: '案内を送る'},
+    ]
+  },
+  methods:{
+  },
+  computed: {
+  }
+})
