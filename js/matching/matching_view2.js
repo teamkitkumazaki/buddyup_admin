27ff6971ca1e5559738614661c0780dbcdabe9b2
Kumazaki Shinnosuke <s.kumazaki@mikke.co.jp>
2019-03-11 14:24:58 +0900

    マッチングの動的部分を追加

--- js/matching/matching_view2.js
+++ js/matching/matching_view2.js
@@ -7,6 +7,19 @@ var app = new Vue({
       {state: false, title: '確認する'},
       {state: false, title: '案内を送る'},
     ],
+    selectItem:[
+      { value: 'state1', name: '近い'},
+      { value: 'state2', name: 'バラバラ'},
+    ],
+    setPeopleNum: true,
+    searchNum: null, // 検索条件 人数
+    searchInterest: false,
+    interestState: 'interest0', // 検索条件 興味関心
+    searchArea: false,
+    areaState: 'area0', // 検索条件 活動範囲
+    searchAge: false,
+    ageState: 'age0', // 検索条件 年齢
+    searchSubmit: false, // 検索結果の表示・非表示
     chartList:[
       {
         interest: 'デザイン',
@@ -47,6 +60,16 @@ var app = new Vue({
     ]
   },
   methods:{
+    searchClick(){
+      if(this.searchNum && this.searchNum >= 3){
+        this.searchSubmit = true
+      }else if(!this.searchNum){
+        alert('マッチング人数を選択してください！');
+      }else if(this.searchNum <= 3){
+        alert('マッチングは3人以上で行ってください！');
+
+      }
+    }
   },
   computed: {
   }
